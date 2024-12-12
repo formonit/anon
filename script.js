650cff5b9645a010c715e4238f6923af684bab87
Somajit Dey <dey.somajit@gmail.com>
2024-12-12 13:04:37 +0530

    PoC

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const securelayEndpointURL = securelayEndpoint(securelayEndpointID)[0];
 
 const formActionURL = securelayEndpointURL + '/public/' + securelayPubKey;
 
+let visitorID;
+let chatURL;
+
 // Brief: Returns the first 5 hex chars from a v4 UUID as a unique string
 function newVisitorID () {
   const id = crypto.randomUUID().substr(0,5);
@@ -13,36 +16,74 @@ function newVisitorID () {
   return id;
 }
 
-const visitorID = localStorage.getItem('visitorID') ?? newVisitorID();
-const chatURL = formActionURL + '/' + visitorID;
+function logChat (msg, received=true) {
+  const chatbox = document.getElementById('chatbox');
+  const row = document.createElement('p');
+  let sender = 'You';
+  if(received) sender = 'Me';
+  const entry = `${sender}[${Date()}]: ${msg}`
+  row.append(entry);
+  chatbox.prepend(row);
+}
+
+async function loadReply () {
+  await fetch(chatURL)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        return response.json();
+      })
+      .then((data) => data['Message'])
+      .then((reply) => { 
+        logChat(reply);
+      })
+      .catch((err) => true);
+}
 
 function setupForm () {
   const contactForm = document.forms.contact;
   const checkImgURL = 'https://img.icons8.com/color/30/approval--v1.png';
   const crossImgURL = 'https://img.icons8.com/emoji/30/cross-mark-emoji.png';
   const query = `?ok=${encodeURIComponent(checkImgURL)}&err=${encodeURIComponent(crossImgURL)}`;
-  contactForm.elements['chatID'].value = visitorID;
+  contactForm.elements['ChatID'].value = visitorID;
   contactForm.action = formActionURL + query;
+  
+  contactForm.addEventListener('submit', async (event) => {
+    const thisForm = event.target;
+    await loadReply();
+    const msg = thisForm.elements['Message'].value;
+    logChat(msg, false);
+  })
 }
 
-function logChat (msg, received=true) {
-  const chatbox = document.getElementById('chat');
-  const row = document.createElement('p');
-  let sender = 'You';
-  if(received) sender = 'Me';
-  const entry = `${sender}[${Date()}]: ${msg}`
-  row.append(entry);
-  logs.prepend(row);
-}
-
-async function loadReply () {
-  const reply = await fetch(chatURL)
-      .then((response) => {
-        if (!response.ok) throw new Error(response.status);
-        return response.json();
+async function init () {
+  visitorID = localStorage.getItem('visitorID');
+  const isRevisit = visitorID ?? false;
+  if (! isRevisit) {
+    const viewCounterFormID = document.forms['viewCounter'].elements['FormID'].value;
+    try {
+      const response = await fetch(formActionURL, {
+        method: 'POST',
+        body: JSON.stringify({ FormID: viewCounterFormID }),
+        headers: {'Content-Type': 'application/json'}
       })
-      .then((data) => data['Message']);
-  logChat(reply);
+      if (response.status != 200) throw new Error();
+      visitorID = newVisitorID();
+    } catch (err) {
+      spaShow('404');
+      console.error(err);
+      return false;
+    }
+  }
+  
+  setupForm();
+  spaShow('form');
+  
+  chatURL = formActionURL + '/' + visitorID;
+  await loadReply();
+  document.getElementById('loadReply').addEventListener('click', (event) => {
+    loadReply();
+  })
+  spaShow('chat');
 }
 
-setupForm();
+init();
